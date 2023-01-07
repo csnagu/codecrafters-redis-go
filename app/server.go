6e51085ebf646e8312558ecb535bec7565f7579d
csnagu <s.chikara3110@gmail.com>
2023-01-07 07:18:09 +0000

    Implement ping-pong

--- app/server.go
+++ app/server.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
+	"io"
 	"net"
 	"os"
 )
@@ -11,16 +11,27 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go pong(conn)
+	}
+}
+
+func pong(conn net.Conn) {
+	defer conn.Close()
+	_, err := io.WriteString(conn, "+PONG\r\n")
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return
 	}
 }
